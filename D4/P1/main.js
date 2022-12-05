1355afb3bce774181128b021005eb90fd425b917
Tristan Dickson <tristan.dickson@efficioconsulting.com>
2022-12-05 14:27:02 +0000

    Day 4

--- D4/P1/main.js
+++ D4/P1/main.js
@@ -3,33 +3,61 @@ var path = require("path");
 
 let inputPath = path.join(__dirname, "input.csv");
 let data = fs.readFileSync(inputPath, "utf8");
-let list = data.split("\n");
+let list = data.split("\n\n");
 
-let h = 0;
-let v = 0;
-
-for (let i = 0; i < list.length; i++) {
-  let instruction = list[i];
-  let direction = instruction[0];
-  let distance = parseInt(instruction[instruction.length - 1]);
-  switch (direction) {
-    case "f":
-      h += distance;
-      break;
-    case "b":
-      h -= distance;
-      break;
-    case "u":
-      v -= distance;
-      break;
-    case "d":
-      v += distance;
-      break;
-
-    default:
-      break;
+const splitLine = (line) => {
+  let lineArray = [];
+  for (let i = 0; i < line.length; i += 3) {
+    lineArray.push(parseInt(line.slice(i, i + 2)));
   }
-}
+  return lineArray;
+};
 
-console.log(`${v}, ${h}`);
-console.log(v * h);
+const checkLine = (numbers, line) => {
+  for (const element of line) {
+    if (!numbers.includes(element)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const checkSubBoard = (numbers, board) => {
+  for (const line of board) {
+    if (checkLine(numbers, line)) {
+      return true;
+    }
+  }
+};
+
+const checkBoard = (numbers, board) => {
+  let tBoard = [...board[0]].map((col, i) => board.map((row) => row[i]));
+  if (checkSubBoard(numbers, board)) return true;
+  if (checkSubBoard(numbers, tBoard)) return true;
+  return false;
+};
+
+const findWinner = (draws, boards) => {
+  for (let i = 5; i < draws.length; i++) {
+    _draws = draws.slice(0, i);
+    for (const board of boards) {
+      if (checkBoard(_draws, board)) return { winner: board, numbers: _draws };
+    }
+  }
+};
+
+const checkScore = (board, numbers) => {
+  const unmarked = board
+    .flat()
+    .reduce((acc, curr) => (!numbers.includes(curr) ? acc + curr : acc), 0);
+
+  return unmarked * numbers[numbers.length - 1];
+};
+
+let draws = list[0].split(",").map((x) => parseInt(x));
+let boards = list.slice(1).map((x) => x.split("\n").map((y) => splitLine(y)));
+
+const { winner, numbers } = findWinner(draws, boards);
+
+console.log(winner);
+console.log(checkScore(winner, numbers));
